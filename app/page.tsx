149e0da4db7fe1c4580e86bf17c4960dd3661234
George Halterman <113994972+Agoews@users.noreply.github.com>
2024-02-20 19:35:10 -0800

    added navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,39 @@
 // import Image from "next/image";
+"use client";
 import Background from "@/public/Background_1.png";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 
 export default function Home() {
+  const { data: session } = useSession();
+
+  if (session && session.user) {
+    return (
+      <main className="flex min-h-screen min-w-screen flex-col items-center">
+        <div
+          className="hero min-h-screen"
+          style={{
+            backgroundImage: `url(${Background.src})`,
+          }}
+        >
+          <div className="hero-overlay bg-opacity-60"></div>
+          <div className="hero-content text-center text-slate-800">
+            <div className="max-w-md">
+              <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+              <p className="mb-5 text-slate-200">
+                Weclome {session.user.name?.split(" ", 1)}
+              </p>
+              <Link href="/tracker">
+                <button className="btn bg-slate-800 text-slate-200">
+                  Get Started
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </main>
+    );
+  }
   return (
     <main className="flex min-h-screen min-w-screen flex-col items-center">
       <div
@@ -16,13 +47,12 @@ export default function Home() {
           <div className="max-w-md">
             <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
             <p className="mb-5 text-slate-200">
-              This is a work in progress! This will be the login page soon
-              enough but that is for future development.
+              Welcome to your all in one trade tracker. This is a work in
+              progress! This will be the login page soon enough but that is for
+              future development.
             </p>
             <Link href="/api/auth/signin">
-              <button className="btn bg-slate-800 text-slate-200">
-                Get Started
-              </button>
+              <button className="btn bg-slate-800 text-slate-200">Login</button>
             </Link>
           </div>
         </div>
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,11 +1,76 @@
+"use client";
+import { signIn, signOut, useSession } from "next-auth/react";
 import React from "react";
-import SignInButton from "../auth/SignInButton";
 
 const Navbar = () => {
+  const { data: session } = useSession();
+
   return (
-    <header className="flex gap-4 bg-gradient-to-b from-white to gray-200 shadow">
-      <SignInButton />
-    </header>
+    <>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h7"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {session && session.user ? (
+                <li>
+                  <a onClick={() => signOut()}>Sign Out</a>
+                </li>
+              ) : (
+                <li>
+                  <a onClick={() => signIn()}>Sign In</a>
+                </li>
+              )}
+              <li>
+                <a>Homepage</a>
+              </li>
+              <li>
+                <a>Portfolio</a>
+              </li>
+              <li>
+                <a>About</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="navbar-center">
+          <a className="btn btn-ghost text-xl">TradeTracker</a>
+        </div>
+
+        <div className="navbar-end">
+          {session && session.user ? (
+            <div className="flex gap-4 ml-auto">
+              <p>{session.user.name}</p>
+            </div>
+          ) : (
+            <></>
+          )}
+        </div>
+      </div>
+    </>
   );
 };
 
