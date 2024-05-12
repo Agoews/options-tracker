97e640e1e34be1bd1ca934db099c15b1abcd1fee
George Halterman <113994972+Agoews@users.noreply.github.com>
2024-05-12 14:26:35 -0700

    added delete route for current stocks

---
+++ app/api/delete-current-holding/route.ts
@@ -0,0 +1,15 @@
+import { sql } from "@vercel/postgres";
+import { NextResponse } from "next/server";
+
+export async function DELETE(request: Request) {
+  try {
+    const { currentstockholdingsid } = await request.json();
+
+    const result =
+      await sql`DELETE FROM currentstockholdings WHERE currentstockholdingsid = ${currentstockholdingsid}`;
+
+    return NextResponse.json({ result }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
