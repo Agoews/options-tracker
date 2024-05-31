cec3b3c205b7a1297f7aee942e15dd9610dca9dc
George Halterman <113994972+Agoews@users.noreply.github.com>
2024-05-30 22:59:13 -0700

    updated assigning CSP to add to current holdings if they exit

--- app/api/assign-current-holdings/route.ts
+++ app/api/assign-current-holdings/route.ts
@@ -45,52 +45,82 @@ export async function POST(request: Request) {
     }
 
     if (actions === "CASH SECURED PUT") {
-      // Add the shares into the CurrentHoldings table
-      const result = await sql`
-        INSERT INTO CurrentStockHoldings (
-          Email,
-          Ticker,
-          Quantity,
-          EntryPrice,
-          TotalValue,
-          CostBasis,
-          OptionsProfit,
-          OpenOptions,
-          MaxOptions,
-          DatePurchased
-        ) VALUES (
-          ${email},
-          ${ticker},
-          ${quantity},
-          ${entryPrice},
-          ${totalValue},
-          ${costBasis},
-          ${numericOptionPrice},
-          0,
-          ${maxOptions},
-          CURRENT_TIMESTAMP
-        );
+      // Check if the record already exists
+      const existingRecord = await sql`
+        SELECT * FROM CurrentStockHoldings
+        WHERE Email = ${email} AND Ticker = ${ticker};
       `;
+
+      if (existingRecord.rowCount > 0) {
+        // Update the existing record
+        const newQuantity = Number(existingRecord.rows[0].quantity) + quantity;
+        const newTotalValue =
+          Number(existingRecord.rows[0].totalvalue) + totalValue;
+        const newCostBasis =
+          (quantity * entryPrice +
+            existingRecord.rows[0].entryprice *
+              existingRecord.rows[0].quantity) /
+          newQuantity;
+        const newMaxOptions = Math.floor(newQuantity / 100) * 100;
+
+        await sql`
+          UPDATE CurrentStockHoldings
+          SET
+            Quantity = ${newQuantity},
+            TotalValue = ${newTotalValue},
+            CostBasis = ${newCostBasis},
+            MaxOptions = ${newMaxOptions}
+          WHERE
+            Email = ${email} AND Ticker = ${ticker};
+        `;
+      } else {
+        // Insert a new record
+        await sql`
+          INSERT INTO CurrentStockHoldings (
+            Email,
+            Ticker,
+            Quantity,
+            EntryPrice,
+            TotalValue,
+            CostBasis,
+            OptionsProfit,
+            OpenOptions,
+            MaxOptions,
+            DatePurchased
+          ) VALUES (
+            ${email},
+            ${ticker},
+            ${quantity},
+            ${entryPrice},
+            ${totalValue},
+            ${costBasis},
+            ${numericOptionPrice},
+            0,
+            ${maxOptions},
+            CURRENT_TIMESTAMP
+          );
+        `;
+      }
 
       // Add closed trade into the ClosedTrades table
       await sql`
-          INSERT INTO ClosedTrades (TradeID, ClosingPrice, CompletionDate, ClosedQuantity)
-          VALUES (${tradeid}, 0, CURRENT_TIMESTAMP, ${openquantity});
-        `;
+        INSERT INTO ClosedTrades (TradeID, ClosingPrice, CompletionDate, ClosedQuantity)
+        VALUES (${tradeid}, 0, CURRENT_TIMESTAMP, ${openquantity});
+      `;
 
       // Update the open quantity in OpenTrades table
       await sql`
-          UPDATE OpenTrades
-          SET openquantity = 0
-          WHERE tradeid = ${tradeid};
-        `;
+        UPDATE OpenTrades
+        SET openquantity = 0
+        WHERE tradeid = ${tradeid};
+      `;
 
       // Check if the open quantity is now 0 or below, and mark the trade as closed
       await sql`
-          UPDATE OpenTrades
-          SET isClosed = TRUE
-          WHERE tradeid = ${tradeid} AND openquantity <= 0;
-        `;
+        UPDATE OpenTrades
+        SET isClosed = TRUE
+        WHERE tradeid = ${tradeid} AND openquantity <= 0;
+      `;
     }
 
     return NextResponse.json(
